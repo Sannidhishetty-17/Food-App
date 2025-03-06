d08b4c03d09f5108a94460a64540f82bcd51135a
Sannidhi Shetty <sannidhii.1609@gmail.com>
2025-03-06 22:50:36 +0530

    Add files via upload

---
+++ src/components/Foodlist.jsx
@@ -0,0 +1,11 @@
+import FoodItem from "./Fooditem";
+
+export default function Foodlist({ foodData, setFoodId }) {
+  return (
+    <div>
+      {foodData.map((food) => (
+        <FoodItem setFoodId={setFoodId} key={food.id} food={food} />
+      ))}
+    </div>
+  );
+}
